4add4143a34819a0838de08d10c25495d9f143e6
Nikolay Kovenko <nikolay.kovenko@gmail.com>
2015-02-10 12:22:12 +0200

    Рефакторинг модели администратора, шифрование пароля

--- models/User.php
+++ models/User.php
@@ -31,6 +31,26 @@ class User extends ActiveModel implements \yii\web\IdentityInterface
     /**
      * @inheritdoc
      */
+    public function init()
+    {
+        parent::init();
+        $this->on(static::EVENT_BEFORE_INSERT, function () {
+            $this->replacePasswordByItsHash();
+        });
+
+        $this->on(static::EVENT_BEFORE_UPDATE, function () {
+            if (empty($this->password_hash) and empty($this->password_hash_repeat)) {
+                $this->password_hash = $this->getOldAttribute('password_hash');
+                $this->password_hash_repeat = $this->password_hash;
+            } else {
+                $this->replacePasswordByItsHash();
+            }
+        });
+    }
+
+    /**
+     * @inheritdoc
+     */
     public static function findIdentity($id)
     {
         return static::findOne($id);
@@ -81,9 +101,9 @@ class User extends ActiveModel implements \yii\web\IdentityInterface
      */
     public function rules()
     {
-//        TODO: Добавить тесты валидации модели
         return [
-            [['username', 'password_hash', 'password_hash_repeat'], 'required'],
+            [['username'], 'required'],
+            [['password_hash', 'password_hash_repeat'], 'required', 'on' => 'create'],
             [['username', 'password_hash', 'password_hash_repeat'], 'string', 'max' => 255],
             [['password_hash'], 'compare', 'compareAttribute' => 'password_hash_repeat'],
         ];
@@ -130,36 +150,22 @@ class User extends ActiveModel implements \yii\web\IdentityInterface
     {
         return \Yii::$app->security->validatePassword($password, $this->password_hash);
     }
+    
+    
+    
 
     /**
-     * @inheritdoc
+     * Зашифровывает пароль
+     * @return bool
      */
-    public function update($runValidation = true, $attributeNames = null)
+    private function replacePasswordByItsHash()
     {
-//        TODO: Провести рефакторинг, function beforeSave
-        if (empty($this->password_hash) and empty($this->password_hash_repeat)) {
-            $this->password_hash = $this->getOldAttribute('password_hash');
-            $this->password_hash_repeat = $this->password_hash;
-        } elseif (!empty($this->password_hash) and $this->password_hash == $this->password_hash_repeat) {
-            $this->password_hash = \Yii::$app->helpers->generatePasswordHash($this->password_hash);
-            $this->password_hash_repeat = $this->password_hash;
-        }
-        
-        return parent::update($runValidation, $attributeNames);
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function insert($runValidation = true, $attributes = null)
-    {
-//        TODO: Провести рефакторинг, function beforeSave
-        
         if (!empty($this->password_hash) and $this->password_hash == $this->password_hash_repeat) {
             $this->password_hash = \Yii::$app->helpers->generatePasswordHash($this->password_hash);
             $this->password_hash_repeat = $this->password_hash;
+            return true;
         }
         
-        return parent::insert($runValidation, $attributes);
+        return false;
     }
 }
